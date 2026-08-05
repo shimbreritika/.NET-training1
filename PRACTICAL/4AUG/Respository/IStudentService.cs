93f15a338c807500fd5cb67f4a5c6c0a1aec160f
Ritika Shimbre <shimbreritika@gmail.com>
2026-08-05 10:33:16 +0530

    Add files via upload

---
+++ PRACTICAL/4AUG/Respository/IStudentService.cs
@@ -0,0 +1,16 @@
+﻿using _4AUG.Models;
+using System.Xml.Schema;
+
+namespace _4AUG.Respository
+{
+    public interface IStudentService
+    {
+        List<Student> GetAll();
+
+        Student? GetStudent (int id);
+        void AddStudent(Student student);
+        void UpdateStudent(Student student);
+        void DeleteStudent(int id);
+       
+    }
+}
