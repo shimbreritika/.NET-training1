a5bf79089c91cf9acbe62927b9c62b39deb3e476
Ritika Shimbre <shimbreritika@gmail.com>
2026-08-09 19:54:58 +0530

    Add files via upload

---
+++ PRACTICAL/4AUG/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using _4AUG.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _4AUG.Data
+{
+    public class AppDbContext : DbContext //from entity framework
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+       
+    }
+}
---
+++ PRACTICAL/4AUG/Migrations/20260808040923_AddOrderRelationships.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _4AUG.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderRelationships : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
